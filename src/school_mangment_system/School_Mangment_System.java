d4fa20f88e9c6af952c67427bdaaf5df1f364bdd
Ahmedsolimanamin <78234531+ahmedsolimanelkomy@users.noreply.github.com>
2023-12-08 15:35:29 +0200

    Add files via upload

---
+++ src/school_mangment_system/School_Mangment_System.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package school_mangment_system;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author ahmed soliman elkomy
+ */
+public class School_Mangment_System {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//         TODO code application logic here
+        JFrame f = new login();
+        f.setVisible(true);
+        f.setResizable(false);
+        f.setLocation(300, 150);
+        
+                
+    }
+    
+}
